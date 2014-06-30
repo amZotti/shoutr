3842ceb58d39a2f407f241efcec7c041011cb357
Anthony Zotti <theillbehaviored@gmail.com>
2014-06-30 10:54:29 -0400

    Enabled shout messages and root changes depending on if user signed in or signed out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
+require "monban/constraints/signed_in"
+
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :shouts, only: [:create]
+
+  constraints Monban::Constraints::SignedIn.new do
+    get "/", to: "dashboards#show", as: :dashboard
+  end
 
   root to: "sessions#new"
+  get "/sign_up", to: "users#new"
 
-  get "/sign_up" => "users#new"
+
 
 end
